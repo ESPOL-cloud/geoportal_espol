f0cba74eb928649ce3b28ecab9063abda1f809a8
juanfraanaleman-cloud <juanfraanaleman@gmail.com>
2026-07-27 10:45:15 -0500

    update

--- main.js
+++ main.js
@@ -266,44 +266,46 @@ const poligonos = new ol.layer.Vector({
       return null; // Oculta el polígono si el campo está vacío o es nulo
     }
 
-    if (zonaValue == 1) {
+    if (zona == 1) {
       return poligonosStyle1; 
     }
-    else if (zonaValue == 2) {
+    else if (zona == 2) {
       return poligonosStyle2; 
     }
-    else if (zonaValue == 3) {
+    else if (zona == 3) {
       return poligonosStyle3; 
     }
-    else if (zonaValue == 4) {
+    else if (zona == 4) {
       return poligonosStyle4; 
     }
-    else if (zonaValue == 5) {
+    else if (zona == 5) {
       return poligonosStyle5; 
     }
-    else if (zonaValue == 6) {
+    else if (zona == 6) {
       return poligonosStyle6; 
     }
-    else if (zonaValue == 7) {
+    else if (zona == 7) {
       return poligonosStyle7; 
     }
-    else if (zonaValue == 8) {
+    else if (zona == 8) {
       return poligonosStyle8; 
     }
-    else if (zonaValue == 9) {
+    else if (zona == 9) {
       return poligonosStyle9; 
     }
-    else if (zonaValue == 10) {
+    else if (zona == 10) {
       return poligonosStyle10; 
     }
-    else if (zonaValue == 11) {
+    else if (zona == 11) {
       return poligonosStyle11; 
     }
-    else if (zonaValue == 12) {
+    else if (zona == 12) {
       return poligonosStyle12; 
-    }else if (zonaValue == 13) {
+    }
+    else if (zona == 13) {
       return poligonosStyle13; 
-    }else if (zonaValue == 14) {
+    }
+    else if (zona == 14) {
       return poligonosStyle14; 
     }
 
