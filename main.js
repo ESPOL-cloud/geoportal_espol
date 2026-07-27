d4e93dbc8b907e638f3b6270a54327a6e76244e3
juanfraanaleman-cloud <juanfraanaleman@gmail.com>
2026-07-27 10:44:21 -0500

    update

--- main.js
+++ main.js
@@ -115,16 +115,198 @@ const poligonosStyle = new ol.style.Style({
   })
 });
 
+const poligonosStyle1 = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(109, 100, 52, 0.62)'    // El 0.5 se refiere a la opacidad
+  }),
+  stroke: new ol.style.Stroke({ 
+    color: '#17191a', 
+    width: 2 
+  })
+});
+
+const poligonosStyle2 = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(19, 114, 32, 0.9)'  
+  }),
+  stroke: new ol.style.Stroke({ 
+    color: '#17191a', 
+    width: 2 
+  })
+});
+
+const poligonosStyle3 = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(79, 175, 92, 0.9)'    
+  }),
+  stroke: new ol.style.Stroke({ 
+    color: '#17191a', 
+    width: 2 
+  })
+});
+
+const poligonosStyle4 = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(26, 67, 128, 0.9)'   
+  }),
+  stroke: new ol.style.Stroke({ 
+    color: '#17191a', 
+    width: 2 
+  })
+});
+
+const poligonosStyle5 = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(103, 196, 196, 0.9)'    
+  }),
+  stroke: new ol.style.Stroke({ 
+    color: '#17191a', 
+    width: 2 
+  })
+});
+
+const poligonosStyle6 = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(15, 15, 15, 0.9)'    
+  }),
+  stroke: new ol.style.Stroke({ 
+    color: '#17191a', 
+    width: 2 
+  })
+});
+
+const poligonosStyle7 = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(196, 95, 28, 0.9)'  
+  }),
+  stroke: new ol.style.Stroke({ 
+    color: '#17191a', 
+    width: 2 
+  })
+});
+
+const poligonosStyle8 = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(240, 228, 57, 0.9)'   
+  }),
+  stroke: new ol.style.Stroke({ 
+    color: '#17191a', 
+    width: 2 
+  })
+});
+
+const poligonosStyle9 = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(103, 75, 139, 0.9)'  
+  }),
+  stroke: new ol.style.Stroke({ 
+    color: '#17191a', 
+    width: 2 
+  })
+});
+
+const poligonosStyle10 = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(214, 24, 10, 0.9)'    
+  }),
+  stroke: new ol.style.Stroke({ 
+    color: '#17191a', 
+    width: 2 
+  })
+});
+
+const poligonosStyle11 = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(120, 127, 128, 0.9)'    
+  }),
+  stroke: new ol.style.Stroke({ 
+    color: '#17191a', 
+    width: 2 
+  })
+});
+
+const poligonosStyle12 = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(41, 79, 138, 0.9)'    
+  }),
+  stroke: new ol.style.Stroke({ 
+    color: '#17191a', 
+    width: 2 
+  })
+});
+
+const poligonosStyle13 = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(126, 185, 111, 0.9)'    
+  }),
+  stroke: new ol.style.Stroke({ 
+    color: '#17191a', 
+    width: 2 
+  })
+});
+
+const poligonosStyle14 = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'rgba(196, 64, 178, 0.9)'    
+  }),
+  stroke: new ol.style.Stroke({ 
+    color: '#17191a', 
+    width: 2 
+  })
+});
+
+
 const poligonos = new ol.layer.Vector({
   source: new ol.source.Vector({ url: './capas/polig_espol.geojson', format: new ol.format.GeoJSON() }),
   visible: false,
   style: function(feature) {
     const attributeValue = feature.get('propietario'); 
-    if (attributeValue !== null && attributeValue !== undefined && String(attributeValue).trim() !== '') {  // Quita las celdas null
-      return poligonosStyle;
-    } else {
+    const zona = feature.get('zona');
+    if (attributeValue === null && attributeValue === undefined && String(attributeValue).trim() === '') {  // Quita las celdas null
       return null; // Oculta el polígono si el campo está vacío o es nulo
     }
+
+    if (zonaValue == 1) {
+      return poligonosStyle1; 
+    }
+    else if (zonaValue == 2) {
+      return poligonosStyle2; 
+    }
+    else if (zonaValue == 3) {
+      return poligonosStyle3; 
+    }
+    else if (zonaValue == 4) {
+      return poligonosStyle4; 
+    }
+    else if (zonaValue == 5) {
+      return poligonosStyle5; 
+    }
+    else if (zonaValue == 6) {
+      return poligonosStyle6; 
+    }
+    else if (zonaValue == 7) {
+      return poligonosStyle7; 
+    }
+    else if (zonaValue == 8) {
+      return poligonosStyle8; 
+    }
+    else if (zonaValue == 9) {
+      return poligonosStyle9; 
+    }
+    else if (zonaValue == 10) {
+      return poligonosStyle10; 
+    }
+    else if (zonaValue == 11) {
+      return poligonosStyle11; 
+    }
+    else if (zonaValue == 12) {
+      return poligonosStyle12; 
+    }else if (zonaValue == 13) {
+      return poligonosStyle13; 
+    }else if (zonaValue == 14) {
+      return poligonosStyle14; 
+    }
+
   }
 });
 
